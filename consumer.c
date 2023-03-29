42e8831987414a6448ea1e6af33e323d7273716a
vscicchi <128091367+vscicchi@users.noreply.github.com>
2023-03-29 15:12:18 -0400

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -2,5 +2,12 @@
 #include <iostream>
 
 int main() {
-  
+  int nextConsumed;
+  while (true) {
+    while (count == 0)
+      ;
+    next_consumed = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
+    count--;
+  }
 }
