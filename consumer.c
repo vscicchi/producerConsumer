0044c014864399d8c684a18946fb74e93fec9698
vscicchi <128091367+vscicchi@users.noreply.github.com>
2023-03-30 16:29:29 -0400

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -1,13 +1,42 @@
 #include "buffer.h"
-#include <iostream>
 
 int main() {
-  int nextConsumed;
-  while (true) {
-    while (count == 0)
-      ;
-    next_consumed = buffer[out];
-    out = (out + 1) % BUFFER_SIZE;
-    count--;
+  const char* sem1 = "notEmpty";
+  const char* sem2 = "notFull";
+  const char* sem3 = "mutex";
+  sem_t* notEmpty, notFull, mutex; // Shared semaphores between the processes (linked to semaphores in the buffer)
+  int* shelf;
+  int shared; // Locates share memory file and describes what is in it
+  int loop = 2; // Loop program twice (max buffer size)
+  
+  shared = shm_open("buffer", O_CREAT | O_RDWR, 0666)
+  
+  // Map memory in process address
+  shelf = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shared, 0);
+  
+  // Create Semaphores
+  notEmpty = sem_open(sem1, O_CREAT, 0666, 0);
+  notFull = sem_open(sem2, O_CREAT, 0666, 3);
+  mutex = sem_open(sem3, OCREAT, 0666, 1);
+  while (loop--) { // Until loop finishes...
+    sem_wait(notEmpty);
+    sem_wait(mutex);
+    (*shelf)--;
+    sem_post(mutex);
+    printf("Item Consumed\n");
+    sem_post(notFull);
   }
+  
+  // Remove and Detach Semaphores
+  sem_close(notEmpty);
+  sem_close(notFull);
+  sem_close(mutex);
+  sem_unlink(sem1);
+  sem_unlink(sem2);
+  sem_unlink(sem3);
+  
+  munmpa(shelf, sizeof(int));
+  close(shared);
+  shm_unlink("buffer");
+  return 0;
 }
