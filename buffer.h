83284260ebb74ca301500c1a7e10bc02065e0f10
vscicchi <128091367+vscicchi@users.noreply.github.com>
2023-03-30 16:20:21 -0400

    Update buffer.h

--- buffer.h
+++ buffer.h
@@ -5,10 +5,10 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/shm.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 
 #define BUFFER_SIZE 2 // Buffer Size of 2
