1abef80b6b400cd57f4bba7714adad40a9ad27b6
vscicchi <128091367+vscicchi@users.noreply.github.com>
2023-04-03 16:38:25 -0400

    Consumer created (problems remain)

--- consumer.c
+++ consumer.c
@@ -1,43 +1,41 @@
 #include "buffer.h"
 
 int main() {
-  const char* sem1 = "notEmpty";
-  const char* sem2 = "notFull";
-  const char* sem3 = "mutex";
-  sem_t* notEmpty, notFull, mutex; // Shared semaphores between the processes (linked to semaphores in the buffer)
-  int* shelf;
-  int shared; // Locates share memory file and describes what is in it
-  int loop = 2; // Loop program twice (max buffer size)
+  // Print instructions
+  printf("1 to Produce Item\n2 to Consume Item\n0 to Exit Program\n\n");
+
+  int shelf = 0;
+  int value = 99; // I/O from user (Arbitrary initialize value)
   
-  shared = shm_open("buffer", O_CREAT | O_RDWR, 0666)
-  
-  // Map memory in process address
-  shelf = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shared, 0);
-  
-  // Create Semaphores
-  notEmpty = sem_open(sem1, O_CREAT, 0666, 0);
-  notFull = sem_open(sem2, O_CREAT, 0666, 3);
-  mutex = sem_open(sem3, OCREAT, 0666, 1);
-  while (loop--) { // Until loop finishes...
-    sem_wait(notEmpty);
-    sleep(rand()%2+1); // Wait for other processes
-    sem_wait(mutex);
-    (*shelf)--; // Remove item from buffer
-    sem_post(mutex);
-    printf("Item Consumed\n");
-    sem_post(notFull);
+  while (value != 0) { // Until loop finishes...
+    printf("Enter Instruction: ");
+    scanf("%d", value);
+    if (value == 2) {
+      sem_wait(&sh_buff.full);
+      sleep(rand()%2+1); // Wait for other processes
+      if (sh_buff.buffer[0] != 0) {
+        if (sh_buff.buffer[1] != 0) {
+          sh_buff.buffer[1] = 0;
+          shelf--;
+        }
+        else {
+          sh_buff.buffer[0] = 0;
+          shelf--;
+        }
+        printf("Item Consumed\n");
+      }
+      else {
+        printf("Buffer Empty!\n");
+      }
+      printf("Item Consumed\n");
+      sem_post(&sh_buff.empty);
+    }
   }
   
   // Remove and Detach Semaphores
-  sem_close(notEmpty);
-  sem_close(notFull);
-  sem_close(mutex);
-  sem_unlink(sem1);
-  sem_unlink(sem2);
-  sem_unlink(sem3);
+  sem_destroy(&sh_buff.empty);
+  sem_destroy(&sh_buff.full);
   
   munmpa(shelf, sizeof(int));
-  close(shared);
-  shm_unlink("buffer");
   return 0;
 }
